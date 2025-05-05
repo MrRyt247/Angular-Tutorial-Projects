e106f6b74fa2f3ba889cff219b977e5bc4ea420c
Mr. Ryt <125740894+MrRyt247@users.noreply.github.com>
2025-05-05 22:56:34 +0000

    🎉 creates dynamic filter list app

---
+++ dynamic-filter-list/src/app/dynamic-filter-list/dynamic-filter-list.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-filter-list',
+  imports: [CommonModule],
+  templateUrl: './dynamic-filter-list.component.html',
+  styleUrl: './dynamic-filter-list.component.scss'
+})
+export class DynamicFilterListComponent {
+
+}
