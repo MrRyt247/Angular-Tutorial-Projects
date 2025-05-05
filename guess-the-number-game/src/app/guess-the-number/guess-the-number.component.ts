fcfc3377a51d034dbfc1dcf8756f578711b7f365
Mr. Ryt <flaviosobbin04@gmail.com>
2025-05-05 00:03:34 +0000

    :tada: creates guess the number game

---
+++ guess-the-number-game/src/app/guess-the-number/guess-the-number.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-guess-the-number',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './guess-the-number.component.html',
+  styleUrl: './guess-the-number.component.scss'
+})
+export class GuessTheNumberComponent {
+
+}
