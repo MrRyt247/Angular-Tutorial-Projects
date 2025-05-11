b73d7015fcde39e07c1f873061f12811d0018c74
Mr. Ryt <flaviosobbin04@gmail.com>
2025-05-11 04:13:57 +0000

    :sparkles: writes game logic

--- rock-paper-scissors-game/src/app/game/game.component.ts
+++ rock-paper-scissors-game/src/app/game/game.component.ts
@@ -5,8 +5,35 @@ import { Component } from '@angular/core';
   selector: 'app-game',
   imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
+  choices: string[] = ['Rock', 'Paper', 'Scissors'];
+  playerChoice: string | null = null;
+  computerChoice: string | null = null;
+  result: string | null = null;
 
+  getRandomNumber(max: number): number {
+    return Math.floor(Math.random() * 3);
+  }
+
+  determineWinner(player: string, computer: string): string {
+    if (player === computer) return "It's a draw!";
+    if (
+      (player === 'Rock' && computer === 'Scissors') ||
+      (player === 'Scissors' && computer === 'Paper') ||
+      (player === 'Paper' && computer === 'Rock')
+    ) {
+      return 'You win!';
+    } else {
+      return 'You lose!';
+    }
+  }
+
+  play(choice: string) {
+    this.playerChoice = choice;
+    this.computerChoice =
+      this.choices[this.getRandomNumber(this.choices.length)];
+    this.result = this.determineWinner(this.playerChoice, this.computerChoice);
+    }
 }
