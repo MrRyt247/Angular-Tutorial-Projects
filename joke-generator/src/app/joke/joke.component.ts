6842350710866c806db8b9b4225c744f906eb072
Mr. Ryt <flaviosobbin04@gmail.com>
2025-05-12 12:06:25 +0000

    :tada: creates joke generator app

---
+++ joke-generator/src/app/joke/joke.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-joke',
+  imports: [],
+  templateUrl: './joke.component.html',
+  styleUrl: './joke.component.scss'
+})
+export class JokeComponent {
+
+}
