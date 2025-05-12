b7fa00c8385e7921676b6b1a292971c599be9fd3
Mr. Ryt <flaviosobbin04@gmail.com>
2025-05-12 09:31:25 +0000

    :tada: creates faq accordion

---
+++ faq-accordion/src/app/faq-accordion/faq-accordion.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-accordion',
+  imports: [CommonModule],
+  templateUrl: './faq-accordion.component.html',
+  styleUrl: './faq-accordion.component.scss'
+})
+export class FaqAccordionComponent {
+
+}
