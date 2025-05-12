9e9c9ad2a8063fdfebaf3e58693360ebf7136d30
Mr. Ryt <flaviosobbin04@gmail.com>
2025-05-12 23:05:15 +0000

    :tada: creates post infinite-scrolling app

--- infinte-scroll/src/app/app.component.ts
+++ infinte-scroll/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PostComponent } from './components/post/post.component';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PostComponent, PostListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'infinte-scroll';
 }
