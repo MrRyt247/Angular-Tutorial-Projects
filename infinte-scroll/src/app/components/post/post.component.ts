c2d017a4f2d2fad2d9e759d27c2b5dd5499488ca
Mr. Ryt <flaviosobbin04@gmail.com>
2025-05-12 23:24:11 +0000

    :sparkles: adds post template UI

--- infinte-scroll/src/app/components/post/post.component.ts
+++ infinte-scroll/src/app/components/post/post.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   imports: [],
   templateUrl: './post.component.html',
-  styleUrl: './post.component.scss'
+  styleUrl: './post.component.scss',
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  @Input() post!: { title: string; body: string };
+  randomPhotoUrl!: string;
 
+  ngOnInit() {
+    this.genrateRandomPhoto();
+  }
+
+  private genrateRandomPhoto(): void {
+    const randomSeed = this.genrateRandomNumber(1000);
+  }
+
+  private genrateRandomNumber(max: number): number {
+    return Math.floor(Math.random() * max);
+  }
 }
