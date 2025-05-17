87d6e2b9d23d357b02ee0ae16d743dafce90bff1
Mr. Ryt <flaviosobbin04@gmail.com>
2025-05-17 03:35:21 +0000

    :sparkles: adds clock logic

--- clock/src/app/clock/clock.component.ts
+++ clock/src/app/clock/clock.component.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CLOCK_CONSTANTS as cc} from './clock.constants';
 
 @Component({
   selector: 'app-clock',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './clock.component.html',
   styleUrl: './clock.component.scss'
 })
 export class ClockComponent {
+  // Rotation angles
+  hourrs: number = 0;
+  minutes: number = 0;
+  seconds: number = 0;
+  clockNumers = this.generateClockNumbers();
 
+  generateClockNumbers() {
+    const numbers = [];
+    const centerOffset = cc.CENTER_OFFSET;
+    const radius = cc.RADIUS;
+
+    for (let i = 1; i <= 12; i++) {
+      const angle = (i * cc.DEGREES_PER_HOUR) * cc.DEG_TO_RAD;
+      const x = centerOffset + radius * Math.cos(angle);
+      const y = centerOffset + radius * Math.sin(angle);
+      numbers.push({ number: i, position: { top: `${x}%`, left: y+'%'}});
+    }
+    return numbers;
+  }
 }
