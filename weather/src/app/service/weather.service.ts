13783326f51bc8dc2f0a855d5be2937a10db436e
Mr. Ryt <flaviosobbin04@gmail.com>
2025-08-23 21:46:25 +0000

    :sparkles: adds weather services and logic

--- weather/src/app/service/weather.service.ts
+++ weather/src/app/service/weather.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getWeather(city: string): Observable<any> {
+    return this.http.get(
+      `${this.apiUrl}?q=${city}&appid=${environment.weatherApiKey}&units=metric`
+    );
+  }
 }
